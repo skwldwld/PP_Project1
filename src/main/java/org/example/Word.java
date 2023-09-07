a6a1f1cbbaa8ff6b2536fffe800e50f52da7f8cb
skwldwld <yezi0512@handong.ac.kr>
2023-09-07 14:54:40 +0900

    Commit

---
+++ src/main/java/org/example/Word.java
@@ -0,0 +1,61 @@
+package org.example;
+
+public class Word {
+    private int id;
+    private int level;
+    private String word;
+    private String meaning;
+
+    Word() {} // 비어있는 기본형 생성자
+    Word(int id, int level, String word, String meaning) { // 멤버변수를 파라메터로 받는 생성자
+        this.id = id;
+        this.level = level;
+        this.word = word;
+        this.meaning = meaning;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+
+    @Override
+    public String toString() {
+        String slevel = "";
+        for(int i = 0; i < level; i++) slevel += "*";
+        String str = String.format("%-3s", slevel) // 왼쪽정렬
+                + String.format("%15s", word + "  " + meaning); // 오른쪽정렬
+        return str;
+    }
+
+    public String toFileString() {
+        return this.level + "|" + this.word + "|" + this.meaning;
+    }
+}
---
+++ src/main/java/org/example/WordCRUD.java
@@ -0,0 +1,168 @@
+package org.example;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class WordCRUD implements ICRUD{ // wordCRUD에서 데이터 추가하는 함수
+    ArrayList<Word> list; // <다루고자 하는 데이터 타입> 변수명
+    Scanner s;
+    final String fname = "Dictionary.txt";
+    /*
+    * => 난이도(1,2,3) & 새 단어 입력 :
+    * 뜻 입력 :
+    * 새 단어가 단어장에 추가되었습니다.
+     */
+    WordCRUD(Scanner s){
+        list = new ArrayList<>();
+        this.s = s;
+    }
+    @Override
+    public Object add() { // 사용자에게 입력받기
+        System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
+        int level = s.nextInt();
+        String word = s.nextLine();
+        System.out.print("뜻 입력 : ");
+        String meaning = s.nextLine();
+
+        return new Word(0, level, word, meaning);
+    }
+
+    public void addItem() { // 입력받은 데이터를 리스트에 추가
+        Word one = (Word)add();
+        list.add(one);
+        System.out.println("새 단어가 단어장에 추가되었습니다.");
+    }
+
+    @Override
+    public int update(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public int delete(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public void selectOne(int id) {
+
+    } // ICRUD를 구현하는 구현체
+
+    public void listAll() {
+        System.out.println("-----------------------------");
+        for(int i = 0; i < list.size(); i++) {
+            System.out.print((i+1) + " ");
+            System.out.println(list.get(i).toString());
+        }
+        System.out.println("-----------------------------");
+    }
+
+    public ArrayList<Integer> listAll(String keyword) {
+        ArrayList<Integer> idlist = new ArrayList<>();
+        int j = 0;
+        System.out.println("-----------------------------");
+        for (int i = 0; i < list.size(); i++) {
+            String word = list.get(i).getWord();
+            if (!word.contains(keyword)) continue;
+            System.out.print((j + 1) + " ");
+            System.out.println(list.get(i).toString());
+            idlist.add(i);
+            j++;
+        }
+        System.out.println("-----------------------------");
+        return idlist;
+    }
+
+    public void listAll(int level) {
+        int j = 0;
+        System.out.println("-----------------------------");
+        for (int i = 0; i < list.size(); i++) {
+            int ilevel = list.get(i).getLevel();
+            if (ilevel != level) continue;
+            System.out.print((j + 1) + " ");
+            System.out.println(list.get(i).toString());
+            j++;
+        }
+        System.out.println("-----------------------------");
+    }
+
+    public void updateItem() {
+        System.out.print("=> 수정할 단어 검색 : ");
+        String keyword = s.next();
+        ArrayList<Integer> idlist = this.listAll(keyword);
+        System.out.print("=> 수정할 번호 선택 : ");
+        int id = s.nextInt();
+        s.nextLine();
+        System.out.print("=> 뜻 입력 : ");
+        String meaning = s.nextLine();
+        Word word = list.get(idlist.get(id-1));
+        word.setMeaning(meaning);
+        System.out.println("단어가 수정되었습니다.");
+    }
+
+    public void deleteItem() {
+        System.out.print("=> 삭제할 단어 검색 : ");
+        String keyword = s.next();
+        ArrayList<Integer> idlist = this.listAll(keyword);
+        System.out.print("=> 삭제할 번호 선택 : ");
+        int id = s.nextInt();
+        s.nextLine();
+
+        System.out.print("=> 정말로 삭제하실래요?(Y/n) ");
+        String ans = s.next();
+        if(ans.equalsIgnoreCase("y")) {
+            list.remove((int)idlist.get(id-1));
+            System.out.println("단어가 삭제되었습니다 ");
+        } else
+            System.out.println("취소되었습니다. ");
+    }
+
+    public void loadFile() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fname));
+            String line;
+            int count = 0;
+
+            while(true) {
+                line = br.readLine();
+                if(line == null) break;
+                String data[] = line.split("\\|"); // 백슬래시 두개를 해줘야 문자로 인식함
+                int level = Integer.parseInt(data[0]);
+                String word = data[1];
+                String meaning = data[2];
+                list.add(new Word(0, level, word, meaning));
+                count++;
+            }
+            br.close();
+            System.out.println("=> " + count + "개 로딩 완료!!!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void saveFile() {
+        try {
+            PrintWriter pr = new PrintWriter(new FileWriter(fname));
+            for(Word one : list) { // 데이터 list에서 하나씩 꺼내옴
+                pr.write(one.toFileString() + "\n"); // write는 뒤에 엔터를 넣어주지 않음
+            }
+            pr.close();
+            System.out.println("=> 데이터 저장 완료!!!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void searchLevel() {
+        System.out.print("=> 원하는 레벨은? (1~3) ");
+        int level = s.nextInt();
+        listAll(level);
+    }
+
+    public void searchWord() {
+        System.out.print("=> 원하는 단어는? ");
+        String keyword = s.next();
+        listAll(keyword);
+    }
+}
